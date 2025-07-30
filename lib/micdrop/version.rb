6b002b3840a43bc30422847b4cb165d1d35e08e5
Dominick Johnson <dominick.johnson@tylertech.com>
2025-07-30 20:12:49 +0000

    Initial auto-generated starter

---
+++ lib/micdrop/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Micdrop
+  VERSION = "0.1.0"
+end
---
+++ test/test_micdrop.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestMicdrop < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Micdrop::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
